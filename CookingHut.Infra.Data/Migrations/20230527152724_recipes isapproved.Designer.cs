7348aedadb26ac05be39ac345f2e621bb7493f6a
Daniela Rodrigues <daniela_rodrigues@live.com.pt>
2023-05-27 16:28:35 +0100

    Migration para recipe.isapproved

--- CookingHut.Infra.Data/Migrations/20230527152724_recipes isapproved.Designer.cs
+++ CookingHut.Infra.Data/Migrations/20230527152724_recipes isapproved.Designer.cs
@@ -4,14 +4,16 @@ using CookingHut.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CookingHut.Infra.Data.Migrations
 {
     [DbContext(typeof(CookingHutDBContext))]
-    partial class CookingHutDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230527152724_recipes isapproved")]
+    partial class recipesisapproved
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
