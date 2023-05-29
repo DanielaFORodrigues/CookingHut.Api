fabcd96d2863ac0c18937914f5a28e58cbdd15b5
Daniela Rodrigues <daniela_rodrigues@live.com.pt>
2023-05-29 20:59:19 +0100

    Migration rating value

--- CookingHut.Infra.Data/Migrations/20230529195747_rating value.Designer.cs
+++ CookingHut.Infra.Data/Migrations/20230529195747_rating value.Designer.cs
@@ -4,14 +4,16 @@ using CookingHut.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CookingHut.Infra.Data.Migrations
 {
     [DbContext(typeof(CookingHutDBContext))]
-    partial class CookingHutDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230529195747_rating value")]
+    partial class ratingvalue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CookingHut.Infra.Data/Migrations/20230529195747_rating value.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CookingHut.Infra.Data.Migrations
+{
+    public partial class ratingvalue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RatingValue",
+                table: "Ratings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "RatingValue",
+                table: "Ratings",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
